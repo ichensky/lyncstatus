0ad570916d1a7992fb34850cdef8dc3f9c679d0a
unknown <ichensky@AK-LP-006.internal.ameria.de>
2015-12-04 12:54:56 +0200

    Add script for changing randomly lync status

---
+++ scripts/setStatus/setStatus.bat
@@ -0,0 +1,12 @@
+REM Change lync status using program `fortune` every 250 seconds  
+REM To install fortune: 
+REM 1. Install `cygwin`, cygwin `python2.7`
+REM 2. python -m pip install fortune
+REM 3. python -m fortune -u f.txt
+REM
+
+:loop
+bash -c "../../LyncStatus/LyncStatus.exe -n ""$(fortune f.txt )"""
+sleep 250
+goto loop
+pause
