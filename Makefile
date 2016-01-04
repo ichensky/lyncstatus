c45b5e01a4a5f58c6765186e33ccfafb856ec1f6
john <john@debiankeranldev>
2016-01-04 08:07:44 -0500

    add task database recreation task in Makefile

--- Makefile
+++ Makefile
@@ -9,33 +9,4 @@ cfg_usr=etc/usr/$(type)/*.cfg
 include $(cfg)
 -include $(cfg_usr)
 
-pgpass_file=~/.pgpass
-pgpass_user=$(db_hostname):$(db_port):$(db_database):$(db_username):
-
-db_superuserpassword=$(shell cat $(pgpass_file) | \
-	grep "$(db_hostname):$(db_port)" | \
-	grep ":$(db_superusername):" | grep -oE "[^:]+$$")
-
-db_userpassword=$(shell cat $(pgpass_file) | \
-	grep $(pgpass_user) | grep -oE "[^:]+$$")
-
-ssql=psql -h $(db_hostname) -p $(db_port) -U $(db_superusername)
-
-all: db_createuser
-	echo "TODO:"
-
-db_createuser:
-	$(ssql) -c \
-	"create user $(db_username) with password '$(db_userpassword)'"
-
-db_dropuser:
-	$(ssql) -c \
-	"drop user $(db_username)"
-
-db_createdatabase:
-	$(ssql) -c \
-	"create database $(db_database)"
-
-db_granduser:
-	$(ssql) -c \
-	"grant all privileges on database $(db_database) to $(db_username)"
+include build/db.mk
---
+++ gen/db.mk
@@ -0,0 +1,44 @@
+
+
+pgpass_file=~/.pgpass
+pgpass_user=$(db_hostname):$(db_port):$(db_database):$(db_username):
+
+db_superuserpassword=$(shell cat $(pgpass_file) | \
+	grep "$(db_hostname):$(db_port)" | \
+	grep ":$(db_superusername):" | grep -oE "[^:]+$$")
+
+db_userpassword=$(shell cat $(pgpass_file) | \
+	grep $(pgpass_user) | grep -oE "[^:]+$$")
+
+ssql=psql -h $(db_hostname) -p $(db_port) -U $(db_superusername)
+sql=psql -h $(db_hostname) -p $(db_posrt) -U $(db_username) -d $(db_database)
+
+all: 
+	echo "TODO:"
+
+db_create_user:
+	$(ssql) -c \
+	"create user $(db_username) \
+	with password '$(db_userpassword)'"
+
+db_drop_user:
+	$(ssql) -c "drop user $(db_username)"
+
+db_create_database:
+	$(ssql) -c "create database $(db_database)"
+
+db_drop_database: db_kill_connections 
+	$(ssql) -c "drop database if exists $(db_database)"
+
+db_grand_user:
+	$(ssql) -c \
+	"grant all privileges \
+	on database $(db_database) to $(db_username)"
+
+db_kill_connections:
+	$(ssql) -c \
+	"select pg_terminate_backend(pg_stat_activity.pid) \
+	from pg_stat_activity \
+	where pg_stat_activity.datname='$(db_database)'"
+
+db_recreate_database: db_drop_database db_create_database db_grand_user
