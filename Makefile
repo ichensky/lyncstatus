86e0e55fcd06f1cde89056b0862e678681eec8b8
john <john@debkern>
2016-01-04 10:31:52 -0500

    add makefile

---
+++ Makefile
@@ -0,0 +1,9 @@
+# local qa staging prod
+type=local
+
+
+include /etc/template.db.cfg
+include /etc/$(type).db.cfg
+
+all:
+
--- db/initdb.sql
+++ db/initdb.sql
@@ -1,17 +1,29 @@
 CREATE TABLE status(
-       id smallint,
-       name varchar(50)
+id serial PRIMARY KEY,
+name varchar(50)
 );
 
 CREATE TABLE contact(
-       id int PRIMARY KEY,
-       uri varchar(1024)
+id serial PRIMARY KEY,
+uri varchar(1024)
 );
 
+-- Contact state in time moment
 CREATE TABLE contactstate(
-       id int PRIMARY KEY,
-       contactid int,
-       statusid smallint,
-       FOREIGN KEY (contactid) REFERENCES contact(id)
-       FOREIGN KEY (statusid) REFERENCES status(id)
+id serial PRIMARY KEY,
+contactid int,
+statusid int,
+statedate timestamp, -- eps=1min
+FOREIGN KEY (contactid) REFERENCES contact(id)
+FOREIGN KEY (statusid) REFERENCES status(id)
+);
+
+-- Contact state at timeline
+CREATE TABLE contactstateattimeline(
+id serial PRIMARY KEY,
+contactid int,
+statusid int,
+changedstatedate timestamp,
+FOREIGN KEY (contactid) REFERENCES contact(id)
+FOREIGN KEY (statusid) REFERENCES status(id)
 );
