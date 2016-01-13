02704977f027cced9627d9238521fba8756aac85
ichensky <ichensky@live.com>
2016-01-12 20:42:11 -0500

    add backend report app

--- src/backend/src/report/report.go
+++ src/backend/src/report/report.go
@@ -60,15 +60,3 @@ func main() {
 	}
 
 }
-
-//fix state()
-//{
-//	select statusid from state order by changedate limit 1;
-//	if(row == 0) insert into state ( invalid )
-//	else update statusid = invalid...
-//}
-//
-//update state()
-//{
-//	if(select from stateid from )....
-//}
