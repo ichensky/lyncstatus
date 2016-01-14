a10c1fec32fd2c4cddf3776aea34b58d1c36ebaf
ichensky <ichensky@live.com>
2016-01-13 20:05:00 -0500

    add simple server responce

--- gen/backend.mk
+++ gen/backend.mk
@@ -4,22 +4,20 @@ export GOPATH
 backend_cfg=$(backend)/cfg
 backend_collector_cfg=$(backend_cfg)/collector.cfg
 
-backend_collector=collector
 backend_bin=$(backend)/bin
 
 backend_build: backend_build_collector backend_build_report
 
 backend_build_collector:
-	go build -v -o $(bin)/lyncspycollector \
-		$(backend_collector)
+	go build -v -o $(bin)/lyncspycollector collector
 
 backend_build_report:
-	go build -v -o $(bin)/lyncspyreport \
-		$(backend_collector)
+	go build -v -o $(bin)/lyncspyreport report
 
 backend_get_packages:
 	echo $(GOPATH)
-	go get -d -u -v github.com/lib/pq
+	# go get -d -u -v github.com/lib/pq
+	# go get -d -u -v github.com/gorilla/mux
 
 backend_clean:
 	rm -rf ${PWD}/$(backend)/packages
--- src/backend/src/report/report.go
+++ src/backend/src/report/report.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	//_ "github.com/BurntSushi/toml"
 	"configManager"
+	_ "github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	//"io/ioutil"
 	//"errors"
@@ -15,48 +16,126 @@ import (
 	//"pgpass"
 	//"strings"
 	//"time"
+	"html"
+	"net/http"
 )
 
-// checkerr( ...
+var connectionString string
+var db *sql.DB
+
+type Status struct {
+	id          int
+	name        string
+	description string
+}
+
+//--- Erorrs
+
 func checkerr(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
-// main ...
-func main() {
+// ---
 
-	c, err := configManager.ReadDbConfig("db.cfg")
-	checkerr(err)
-	fmt.Println(c.Pgpass)
+// --- Pages
 
-	connectionString := configManager.ConectionString(c.Pgpass)
-	fmt.Println(connectionString)
+// Status ...
+func statusPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("status...page")
 
-	db, err := sql.Open("postgres", connectionString)
-	if err != nil {
-		log.Fatal("Error: The data source arguments are not valid")
-	}
-	defer db.Close()
+}
 
-	err = db.Ping()
+func indexPage(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "<h1>Hello in LyncSpy app.</h1><br />"+
+		"<p>U can see statistic here:<br /></p>")
+	return
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+
+}
+
+// ---
+
+/// --- Data
+
+// pingDb( ...
+func pingDb() {
+
+	err := db.Ping()
 	if err != nil {
 		log.Fatal("Error: Could not establish a connection with the database")
 	}
+}
+
+func statusData() ([]Status, error) {
 
 	rows, err := db.Query("select * from status")
 	checkerr(err)
 	fmt.Println(rows.Columns())
 
+	var status []Status
 	var id int
 	var name string
 	var description string
+
 	for rows.Next() {
 
 		err = rows.Scan(&id, &name, &description)
 		checkerr(err)
-		fmt.Printf("%3v %15v %45v\n", id, name, description)
-	}
 
+		status = append(status, Status{id, name, description})
+
+		//fmt.Printf("%3v %15v %45v\n", id, name, description)
+	}
+	return status, nil
+}
+
+/// ---
+
+/// --- Config
+func initData() {
+
+	c, err := configManager.ReadDbConfig("db.cfg")
+	checkerr(err)
+	connectionString = configManager.ConectionString(c.Pgpass)
+}
+
+/// ---
+
+/// --- Server
+
+// StartServer( ...
+func startServer() {
+
+	log.Println("Server started at: :8080")
+
+	http.HandleFunc("/", indexPage)
+	http.HandleFunc("/", statusPage)
+	log.Fatalln(http.ListenAndServe(":8080", nil))
+
+	log.Println("Servert stoped.")
+
+}
+
+/// ---
+
+// main ...
+func main() {
+	initData()
+	var err error
+	db, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal("Error: The data source arguments are not valid")
+	}
+	defer db.Close()
+
+	pingDb()
+
+	var status []Status
+	status, err = statusData()
+
+	fmt.Println(&status)
+	//startServer()
 }
